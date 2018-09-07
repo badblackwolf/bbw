83b9ba053d97edcb5ee637e6b04ba5ee0f6dca02
badblackwolf <arshiyah52@gmail.com>
2018-09-08 00:02:31 +0430

    Update sbot.lua

--- plugins/sbot.lua
+++ plugins/sbot.lua
@@ -1,5 +1,5 @@
 local function run(msg, matches)
-local bot_m = 159496857
+local bot_s = 159496857
 if matches[1]:lower() == 's' and is_sudo(msg) then
 local function botm(arg, data)
 if data.results_ and data.results_[0] then
@@ -8,7 +8,7 @@ else
 tdcli.sendMessage(msg.chat_id_, msg.id_, 0, 1, nil,':>', 'md')
 end
 end
-tdcli.getInlineQueryResults(bot_m, msg.chat_id_, 0, 0, matches[2], 0, botm, nil)
+tdcli.getInlineQueryResults(bot_s, msg.chat_id_, 0, 0, matches[2], 0, bots, nil)
 end
 end
 return {
